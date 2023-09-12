1a397275eee011ff71cf09a450b9929d36d33104
yaokai <yaokai@dehuinet.com>
2023-09-12 16:03:48 +0800

    引入第三方依赖来对命令行传入的参数进行处理

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
-import { mkdirSync, readFileSync, writeFileSync, existsSync} from 'fs';
-import {fileURLToPath} from 'url';
-import {dirname, resolve} from 'path';
+import { generate } from './lib/generator.js';
+import { createRandomPicker } from './lib/random.js';
+import { saveCorpus, loadCorpus } from './lib/corpus.js';
+import { options } from './lib/cmd.js';
 
-import {generate} from './lib/generator.js';
-import {createRandomPicker} from './lib/random.js';
-import moment from 'moment';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-function loadCorpus(src) {
-  const path = resolve(__dirname, src);
-  const data = readFileSync(path, {encoding: 'utf-8'});
-  return JSON.parse(data);
-}
 
 const corpus = loadCorpus('corpus/data.json');
-
-const pickTitle = createRandomPicker(corpus.title);
-const title = pickTitle();
-
-const article = generate(title, {corpus});
+const title = options.title || createRandomPicker(corpus.title);
+const article = generate(title, { corpus });
 // console.log(`${title}\n\n    ${article.join('\n    ')}`);
 
-saveCorpus(title, article)
-
-
-function saveCorpus(title, article) {
-    const outputDir = resolve(__dirname, 'output');
-    const time = moment().format('|YYYY-MM-DD|HH:mm:ss');
-    const outputFile = resolve(outputDir, `${title}${time}.txt`);
-    // 检查outputDir是否存在，没有则创建一个
-    if(!existsSync(outputDir)) {
-      mkdirSync(outputDir);
-    }
-
-    const text = `${title}\n\n    ${article.join('\n    ')}`;
-    writeFileSync(outputFile, text); // 将text写入outputFile文件中
-
-    return outputFile;
-}
-
-  
-
+const output = saveCorpus(title, article)
+console.log(`生成成功！文章保存于：${output}`);
